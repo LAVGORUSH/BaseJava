3fc3caf425cff967bf4dce190485089fe064521c
Lebedev Anton <lavgorush@gmail.com>
2020-08-18 23:12:26 +0400

    Introduce package and encapsulation

--- src/ru/javawebinar/basejava/storage/ArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/ArrayStorage.java
@@ -1,3 +1,7 @@
+package ru.javawebinar.basejava.storage;
+
+import ru.javawebinar.basejava.model.Resume;
+
 /**
  * Array based storage for Resumes
  */
@@ -19,7 +23,7 @@ public class ArrayStorage {
 
     public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return storage[i];
             }
         }
@@ -28,7 +32,7 @@ public class ArrayStorage {
 
     public void delete(String uuid) {
         for (int i = 0; i < size; i++) {
-            if (uuid.equals(storage[i].uuid)) {
+            if (uuid.equals(storage[i].getUuid())) {
                 storage[i] = storage[size - 1];
                 storage[size - 1] = null;
                 size--;
